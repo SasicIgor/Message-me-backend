1c47d2a790af787f76a30127a4e28fcebfafa2b6
Igor-Sasic <igor.sasic811@gmail.com>
2025-10-30 19:13:02 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import app from "./server.ts";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port http://localhost:${PORT}`);
+});
